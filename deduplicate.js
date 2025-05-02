f82c566399dc940ea9c477b5b3d7b73538829e83
Bryan Elliott <bryan.elliott@cesium.com>
2025-05-02 08:31:19 -0400

    simplify sort

--- deduplicate.js
+++ deduplicate.js
@@ -1,9 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
 const reasons = JSON.parse(readFileSync("./reasons.json", "utf8"));
 const deduplicated = [...new Set(reasons)];
-const sorted = deduplicated.toSorted((a, b) => {
-  a = a.toLowerCase();
-  b = b.toLowerCase();
-  return a < b ? -1 : a > b ? 1 : 0;
-});
+const sorted = deduplicated.toSorted((a, b) => a.toLowerCase() < b.toLowerCase() ? -1 : 1);
 writeFileSync("./reasons.json", JSON.stringify(sorted, null, 2), "utf8");
